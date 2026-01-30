3a2c242525df4c5b4029e6f45bf050449de06fcd
falaprodavi <falaprodavo@gmail.com>
2026-01-30 13:44:22 -0300

    chore: add shadcn setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold text-red-500">Welcome to the Home Page</h1>
 
-    </main>
+      <Button>Olá</Button>
+
   );
 }
